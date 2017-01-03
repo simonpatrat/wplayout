03d83ae2ff15298c94f6d391c33882e5533d9f41
simonpatrat <simon.patrat@gmail.com>
2017-01-03 12:46:27 -0500

    Added setHeights function

--- js/main.js
+++ js/main.js
@@ -12,13 +12,24 @@
         if ($('.column-size').hasClass('col-xs-12') && nbColumns == 6) {
             $('.column-size').removeClass('col-xs-12 col-sm-12 col-md-12')
                              .addClass('col-xs-6 col-sm-6 col-md-6');
+            setHeights('.column-size.col-md-6');
 
         } else if ($('.column-size').hasClass('col-md-6') && nbColumns == 6){
             return;
         } else {
             $('.column-size').removeClass('col-xs-6 col-sm-6 col-md-6')
-                .addClass('col-xs-12 col-sm-12 col-md-12');
+                .addClass('col-xs-12 col-sm-12 col-md-12').css('height', 'auto');
         }
     });
+
+    function setHeights(elementsToSet) {
+        var maxHeight = 0;
+
+        $(elementsToSet).each(function(){
+            if ($(this).height() > maxHeight) { maxHeight = $(this).height(); }
+        });
+
+        $(elementsToSet).css('height', maxHeight + 'px');
+    }
 
 })(jQuery);
